49738187d437c64544f84bebe2a3daa623f4da4b
Alvar <alvarsnow@gmail.com>
2020-03-18 12:33:15 +0000

    inicio binomio

---
+++ binomio.c
@@ -0,0 +1,29 @@
+/*----------------------------------------------+
+|Nombre:   binomio.c                            |
+|Sinopsis: El programa calcula el binomio de    |
+| newton                                        |
+|Descripción:                                   |
+|          c:>10saludo                          |
+|Directivas de compilación: compilador gcc      |
+|Lista de códigos de salida del programa:       |
+|  Devuelve 0.                                  |
+|Organización: Universidad de Burgos            |
+|Autor:   Alvar San Martin                      |
+|Fecha:   18/03/2020                            |
+|Versión: v1.0                                  |
++----------------------------------------------*/
+
+
+#include<stdio.h>
+
+int leeEntero();
+unsigned leeEnteroPositivo();
+void calculaResultado(int,int,unsigned,unsigned);
+long binomial(unsigned,unsigned);
+long factorial(int);
+
+
+int main() {
+  
+  return 0;
+}
